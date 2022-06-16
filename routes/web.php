d69d1df907d98d21fc7d1b173fd5e037a338356a
sweetsica <barondeepsnow@gmail.com>
2022-06-16 09:27:20 +0700

    fix export route

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,4 @@ Route::get('/testmd5',function (){
 //    return base64_encode($data);
 });
 
-Route::get('/nova/export',[QRController::class,'export']);
+Route::get('/export',[QRController::class,'export']);
