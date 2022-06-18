78828165da19ef3b6af1948918607a97568c5f8c
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-06-18 09:12:48 +0700

    update chartjs

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -16,9 +16,11 @@ use Coroowicaksono\ChartJsIntegration\StackedChart;
 use Coroowicaksono\ChartJsIntegration\PieChart;
 use Coroowicaksono\ChartJsIntegration\DoughnutChart;
 use Coroowicaksono\ChartJsIntegration\AreaChart;
+
 class Main extends Dashboard
 {
-    public function name(){
+    public function name()
+    {
         return "Trang thống kê";
     }
 
@@ -33,37 +35,39 @@ class Main extends Dashboard
             (new PieChart())
                 ->title('Thống kê khách hàng')
                 ->series(array([
-                    'data' => [count(Customer::all()), 0,0],
-                    'backgroundColor' => ["#ffcc5c","#91e8e1","#ff6f69","#88d8b0","#b088d8","#d8b088", "#88b0d8", "#6f69ff"],
+                    'data' => [count(Customer::all()), 0, 0],
+                    'backgroundColor' => ["#ffcc5c", "#91e8e1", "#ff6f69", "#88d8b0", "#b088d8", "#d8b088", "#88b0d8", "#6f69ff"],
                 ]))
                 ->options([
                     'xaxis' => [
-                        'categories' => ['Khách hàng mới','Khách hàng cũ', 'Khách hàng nội bộ']
+                        'categories' => ['Khách hàng mới', 'Khách hàng cũ', 'Khách hàng nội bộ']
                     ],
                 ])->width('1/3')
-            ,new QrPerPlan, new OrderCount,(new DoughnutChart())
-                ->title('Thống kê đơn hàng')
+            ,
+            new QrPerPlan,
+            (new DoughnutChart())
+                ->title('Thống kê trả thưởng')
                 ->series(array([
-                    'data' => [count(Order::where('status',"Đang xử lý")->get()), count(Order::where('status',"Đang chuẩn bị hàng")->get()), count(Order::where('status',"Đã xuất kho")->get()),  count(Order::where('status',"Đang giao")->get()), count(Order::where('status',"Hoàn tất")->get())],
-                    'backgroundColor' => ["#ffcc5c","#91e8e1","#ff6f69","#b088d8","#88d8b0"],
+                    'data' => [count(Order::where('status', "Đang xử lý")->get()), count(Order::where('status', "Đang chuẩn bị hàng")->get()), count(Order::where('status', "Đã xuất kho")->get()), count(Order::where('status', "Đang giao")->get()), count(Order::where('status', "Hoàn tất")->get())],
+                    'backgroundColor' => ["#ffcc5c", "#91e8e1", "#ff6f69", "#b088d8", "#88d8b0"],
                 ]))
                 ->options([
                     'xaxis' => [
-                        'categories' => ['Đang xử lý','Đang chuẩn bị','Đã xuất kho','Đang giao','Hoàn tất']
+                        'categories' => ['Đang xử lý', 'Đang chuẩn bị', 'Đã xuất kho', 'Đang giao', 'Hoàn tất']
                     ],
                 ])->width('1/3')
-            ,(new StackedChart())
-            ->title('Thống kê mã khách hàng đã quét theo tháng')
-            ->model('\App\Models\QR')
-            ->options([
-                'queryFilter'=>array([    // add array of filter with this format
-                    'key' => 'status',
-                    'operator' => '=',
-                    'value' => 0
-                ])
-            ])
-            ->width('2/3'),new GiftCount
-
-            ,new ProductCount];
+            , (new StackedChart())
+                ->title('Thống kê mã khách hàng đã quét theo tháng')
+                ->model('\App\Models\QR')
+                ->options([
+                    'queryFilter' => array([    // add array of filter with this format
+                        'key' => 'status',
+                        'operator' => '=',
+                        'value' => 0
+                    ])
+                ]), new GiftCount
+            , new ProductCount,
+            new OrderCount
+        ];
     }
 }
