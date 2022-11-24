123269ffe04f8af01a467add2f454aabf9b9e14b
sweetsica <barondeepsnow@gmail.com>
2022-11-24 16:18:22 +0700

    fix coenzyme seed

--- database/seeders/AZDepotSeeder.php
+++ database/seeders/AZDepotSeeder.php
@@ -20,7 +20,7 @@ class AZDepotSeeder extends Seeder
     {
         $promotion_id = 1;
         $product_id = 1 ;
-        $count = 10000;
+        $count = 20000;
         $timeNow = Carbon::now();
         for ($i =0 ; $i < $count ; $i++){
             $specialCode = Str::random(12);
