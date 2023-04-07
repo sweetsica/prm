a4a54fc77eda1492f5cb03e37f94bc50b674c618
sweetsica <barondeepsnow@gmail.com>
2023-04-08 00:10:44 +0700

    add vitamin E

---
+++ database/seeders/ProductSeeder3_07_03_2023.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder3_07_03_2023 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //        Vitamin E
+        DB::table('products')->insert([
+            'name' =>'Vitamin E',
+            'link_product'=>'https://doppelherz.vn/san-pham/thuc-pham-bao-ve-suc-khoe-doppelherz-aktiv-vitamin-e/',
+            'image'=>'https://doppelherz.vn/wp-content/uploads/2023/04/Vitamin-E-front-01-2.png',
+            'price' => '0',
+            'status' => '1',
+        ]);
+        DB::table('gifts')->insert([
+            'id'=>'35',
+            'name' =>'Phần quà: Vitamin E',
+            'image'=>'https://doppelherz.vn/wp-content/uploads/2023/04/Vitamin-E-front-01-2.png',
+            'typePromotion'=>'point',
+            'valuePromotion'=>'67',
+            'status' => '1'
+        ]);
+        DB::table('promotions')->insert([
+            'name' =>'Tích điểm Vitamin E',
+            'product_id'=>'33',
+            'typeSale'=>'point',
+            'valueSale'=>'4',
+            'status' => '1'
+        ]);
+    }
+}
---
+++ database/seeders/VitaminESeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class VitaminESeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $promotion_id =35;
+        $product_id = 33;
+        $timeNow = Carbon::now();
+        $count = 5000;
+        for ($i =0 ; $i < $count ; $i++){
+            $specialCode = Str::random(12);
+            DB::table('q_r_s')->insert([
+                "promotion_id"=>$promotion_id,
+                "product_id"=>$product_id,
+                "specialCode"=>$specialCode,
+                "linkQR"=>"https://tichdiem.doppelherz.vn/tichdiem/".$promotion_id."/".$product_id."/".$specialCode,
+                'created_at'=>$timeNow,
+                'updated_at'=>$timeNow
+            ]);
+        }
+    }
+}
